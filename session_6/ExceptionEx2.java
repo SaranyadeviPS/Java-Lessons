948695b4feed0b360b173d42b9c4693b1fb927e3
Saranya devi palanisamy <160625785+SaranyadeviPS@users.noreply.github.com>
2025-03-25 15:30:22 +0530

    Add files via upload

---
+++ session_6/ExceptionEx2.java
@@ -0,0 +1,18 @@
+package session_6;
+
+public class ExceptionEx2 {
+public static void main(String[] args) {
+	try {
+		System.out.println("trying to divide by 0...");
+		int res=10/0;
+		System.out.println("Result:"+res);
+		
+	}
+	catch(Exception e) {
+		System.out.println(e.getMessage());
+		e.printStackTrace();
+	}finally {
+		System.out.println("Finally block ...");
+	}
+}
+}
---
+++ session_6/ExceptionEx3.java
@@ -0,0 +1,21 @@
+package session_6;
+
+public class ExceptionEx3 {
+static void validateAge(int age) {
+	if(age<18) {
+		throw new IllegalArgumentException("Age must be 18 or above");
+		
+	}else {
+		System.out.println("Age is valid ");
+	}
+
+}
+public static void main(String[] args) {
+	try {
+		validateAge(15);
+		
+	}catch(IllegalArgumentException e) {
+		System.out.println("Caught an exception:"+e.getMessage());
+	}
+}
+}
